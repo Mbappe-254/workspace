b01f9b6cd079a9020ee11a1cb37587843715686c
ALBERT O MATARA <137197284+Mbappe-254@users.noreply.github.com>
2023-08-28 21:31:24 +0000

    Sandbox snapshot commit

--- features/display/DisplayList.js
+++ features/display/DisplayList.js
@@ -1,1 +1,33 @@
 import { useSelector } from 'react-redux';
+import { Col,Row } from 'reactstrap';
+import AnimatedDisplay from './AnimatedDisplay';
+import { SelectFeaturedCream } from '../icecreams/iceCreamSlice';
+import { SelectFeaturedPromotion } from '../promotionSlice';
+import {selectFeaturedPartner} from '../partners/partnernSlice';
+
+const DisplayList = () => {
+    const items = useSelector((state) => [
+        SelectFeaturedCream(state),
+        selectFeaturedPromotion(state),
+        selectFeaturedPartner(state)
+    ]);
+        
+    console.log('display items',items);
+    
+    return (
+        <Row>
+            {items.map((item, idx) => {
+
+                return (
+                    item && (
+                        <Col md className='m-1' key={idx}>
+                            <AnimatedDisplayCard item={item} />
+                        </Col>
+                    )
+                );
+            })}
+        </Row>
+    );
+};
+
+export default DisplayList;

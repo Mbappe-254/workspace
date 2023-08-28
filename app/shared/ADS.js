5afde960870af42b86f7200fcd98d31740980660
ALBERT O MATARA <137197284+Mbappe-254@users.noreply.github.com>
2023-08-28 19:58:17 +0000

    Sandbox snapshot commit

--- app/shared/ADS.js
+++ app/shared/ADS.js
@@ -6,31 +6,31 @@ import Cherry from '../assets/img/cupCherry.jpeg';
 export const ADS = [
     {
         id: 0,
-        name: 'Mountain Adventure',
-        image: breadCrumbImg,
+        name: 'strawberry',
+        image: strawberry,
         featured: true,
         description:
             'Book a 5-day mountain trek with a seasoned outdoor guide! Fly fishing equipment and lessons provided.'
     },
     {
         id: 1,
-        name: 'Into The Weeds',
-        image: weedsImg,
+        name: 'sundae',
+        image: sundae,
         featured: false,
         description:
             '5 days deep in the Redux National Monument, far from the beaten path, with only a reducer in your backpack. Guide provided.'
     },
     {
         id: 2,
-        name: 'Bootstrap Teardown',
-        image: bootsImg,
+        name: 'together',
+        image: together,
         featured: false,
         description: `In this weekend workshop, we'll rebuild your bootstraps from scratch, so you're ready for next-level excursions.`
     },
     {
         id: 3,
-        name: 'Asynchronous Rendezvous',
-        image: asyncImg,
+        name: 'cherry',
+        image: Cherry,
         featured: false,
         description:
             'Meet up with your guide at the top of Promise Mountain. Or, try to. Good luck!'
---
+++ components/Footer.js
@@ -0,0 +1,77 @@
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+
+    return (
+        <footer className='site-footer'>
+            <Container>
+                <Row>
+                    <Col xs={{ size: 4, offset: 1 }} sm='2'>
+                        <h5>Links</h5>
+                        <ul className='list-unstyled'>
+                            <li>
+                                <Link to='/'>Recipe</Link>
+                            </li>
+                            <li>
+                                <Link to='/directory'>Our Flavors</Link>
+                            </li>
+                            <li>
+                                <Link to='/about'>Contact Us</Link>
+                            </li>
+                            <li>
+                                <Link to='/contact'>Where To Buy</Link>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col xs='6' sm='3' className='text-center'>
+                        <h5>Social</h5>
+                        <a
+                            className='btn btn-social-icon btn-instagram'
+                            href='http://instagram.com/'
+                        >
+                            <i className='fa fa-instagram' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-facebook'
+                            href='http://www.facebook.com/'
+                        >
+                            <i className='fa fa-facebook' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-twitter'
+                            href='http://twitter.com/'
+                        >
+                            <i className='fa fa-twitter' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-google'
+                            href='http://youtube.com/'
+                        >
+                            <i className='fa fa-youtube' />
+                        </a>
+                    </Col>
+                    <Col sm='4' className='text-center'>
+                        <a
+                            role='button'
+                            className='btn btn-link'
+                            href='tel:+16124587917'
+                        >
+                            <i className='fa fa-phone' /> 1-612-458-7917
+                        </a>
+                        <br />
+                        <a
+                            role='button'
+                            className='btn btn-link'
+                            href='mailto:notreal@notreal.co'
+                        >
+                            <i className='fa fa-envelope-o' /> amatara1996@gmail.com
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+
+    );
+}
+export default Footer;
